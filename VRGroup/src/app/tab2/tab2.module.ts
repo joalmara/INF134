689c7d61612fd3d2d08a77be88bda48a74142c24
Jason Almaraz <joalmara@uci.edu>
2019-02-12 13:49:00 -0800

    Added Calendar - Jason

--- VRGroup/src/app/tab2/tab2.module.ts
+++ VRGroup/src/app/tab2/tab2.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { CalendarModule } from 'ion2-calendar';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    IonicModule.forRoot(),
+    CalendarModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
+
 })
 export class Tab2PageModule {}
--- VRGroup/src/app/tab2/tab2.page.ts
+++ VRGroup/src/app/tab2/tab2.page.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  date: string;
+  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  constructor() { }
+
+  onChange($event){
+  	console.log($event);
+  }
+}
